0c14a54c6250629226ec2d90cfdcfee6c4d6ef58
fredvanrijswijk <fred@attiq.nl>
2020-10-20 11:10:49 +0200

    cleaning

--- config/db-admin.ts
+++ config/db-admin.ts
@@ -1,7 +1,8 @@
-// import { compareDesc, compareAsc, parseISO } from 'date-fns';
+
 import { db } from './firebase-admin';
 
 export default async function getAllDishes() {
+
     const snapshot = await db.collection('dishes').get();
   
     const dishes = [];
