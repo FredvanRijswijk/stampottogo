444c3cd169ebbb1addd42846fed0fbf37c24c7d7
fredvanrijswijk <fred@attiq.nl>
2020-10-20 12:07:15 +0200

    robots disallow staging

--- pages/api/checkout_sessions/cart.ts
+++ pages/api/checkout_sessions/cart.ts
@@ -18,9 +18,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2020-08-27',
 })
 
-
-
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
