2c6b3bdebce7ec75c63200fb62859bce06924d5e
fredvanrijswijk <fred@attiq.nl>
2020-11-03 13:39:41 +0100

    menu static

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,16 +1,61 @@
 import React from "react";
 import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+import Cart from "../../components/Cart";
+import Navbar from "../../components/Navbar";
+import Image from "next/image";
+import { useShoppingCart, formatCurrencyString } from 'use-shopping-cart'
 
 // posts will be populated at build time by getStaticProps()
 function Menu({ posts }) {
     return (
-      <ul>
+      <Cart>
+          <Navbar />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8">
         {posts.dishes.map((post) => (
-          <li>
-            <NextLink href={`/menu/${post.sku}`} >{post.name}</NextLink>
-          </li>
+          <>
+          <div className="px-2 py-2 m-8 sm:m-1">
+          <div key={post.sku} className="bg-white border rounded-lg overflow-hidden">
+            <Image src={post.image} alt={post.name} unsized className="object-fill w-full"/>
+            <div className="p-6">
+            <h2 className="font-semibold text-xl uppercase">{post.name}</h2>
+            <div className="flex justify-between">
+              <div className="text-gray-600 text-sm">{post.weight}</div>
+              <div className="text-gray-900 text-lg font-bold">{formatCurrencyString({
+                value: post.price,
+                currency: post.currency,
+                language: 'nl-NL'
+              })}</div>
+            </div>
+            <div className="mt-2 leading-tight">
+              {post.description}
+            </div>
+            <div className="mt-4">
+            <button className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium square-md text-white bg-gray-900 hover:bg-gray-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+              onClick={() => addItem(post)}
+            >
+              Toevoegen
+            </button>
+            {/* <button
+              
+              onClick={() => decrementItem(dishes.sku, 1)}
+            />
+            <button
+              
+              onClick={() => incrementItem(dishes.sku, 1)}
+            />
+            <button 
+              onClick={() => removeItem(dishes.sku)}
+            >
+              Verwijderen
+            </button> */}
+            </div>
+            </div>
+          </div>
+          </div>
+          </>
         ))}
-      </ul>
+      </div>
+      </Cart>
     )
   }
   
