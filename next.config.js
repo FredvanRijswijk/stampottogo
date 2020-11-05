2c1b054c10a8c97e25ac59afd700b417cf86fd36
fredvanrijswijk <fred@attiq.nl>
2020-11-05 10:38:42 +0100

    no redirect

--- next.config.js
+++ next.config.js
@@ -7,24 +7,5 @@ module.exports = withTM({
       domains: ['res.cloudinary.com'],
       path: '/_next/image',
       loader: 'default',
-    },
-    async redirects() {
-      return [
-        {
-          source: '/default.asp?page=Vraag_%20en%20_antwoord_Assistent_manager&pid=46',
-          destination: '/veel-gestelde-vragen',
-          permanent: true
-        },
-        {
-          source: '/default.asp?page=Vraag_%20en%20_antwoord_Activiteiten&pid=80',
-          destination: '/veel-gestelde-vragen',
-          permanent: true
-        },
-        {
-          source: '/default.asp?page=nieuws&pid=5&id=50&c=',
-          destination: '/veel-gestelde-vragen',
-          permanent: true
-        }
-      ];
     }
   });
