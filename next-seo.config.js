25738a479793478e943691c347460385f0f0396e
fredvanrijswijk <fred@attiq.nl>
2020-11-11 10:54:27 +0100

    oeteldonk seo + robots.txt

--- next-seo.config.js
+++ next-seo.config.js
@@ -6,13 +6,13 @@ export default {
       site_name: 'STAMPPOT to go',
       images: [
         {
-          url: 'https://res.cloudinary.com/stamppot-to-go/image/upload/v1604488831/social_stamppottogo_on8rnv.jpg',
+          url: 'https://www.stamppottogo.nl/static/images/oeteldonk.jpg',
           width: 1200,
           height: 630,
           alt: 'STAMPPOT to go',
         },
         {
-          url: 'https://res.cloudinary.com/stamppot-to-go/image/upload/v1604488851/logo_m2v83g.png',
+          url: 'https://www.stamppottogo.nl/static/images/oeteldonk.jpg',
           width: 512,
           height: 512,
           alt: 'STAMPPOT to go',
